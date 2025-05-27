3acfb2a0355f15611392bb63d9e67096de4d7fe5
hardwak <ymhere2@gmail.com>
2025-05-28 01:30:04 +0200

    added postgres sql port

--- fargate/security_groups.tf
+++ fargate/security_groups.tf
@@ -87,6 +87,13 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port       = 5432 #PostgreSQL port
+    to_port         = 5432
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
